a9dde0a34621581b1b9e626032274def1009fcf9
balthazarbk <balthazarberlingkelpe@gmail.com>
2020-12-17 20:30:31 +0100

    second commit

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,14 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="prose">
+    <div className="prose text-center max-w-none mt-20">
       <Head>
         <title>Hudson Lab</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Hudson Lab</h1>
-      <p>Hello</p>
+      <h1>Hudson Lab...</h1>
+      <p>...is in construction.</p>
     </div>
   )
 }
