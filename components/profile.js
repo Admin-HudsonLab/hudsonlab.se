4fa9cabcf9e761fbd6af31cb91022b52a3c98726
balthazarbk <balthazarberlingkelpe@gmail.com>
2020-12-28 16:04:01 +0100

    introduced Image comp from next in People. Should continue.

--- components/profile.js
+++ components/profile.js
@@ -1,14 +1,16 @@
 import Link from "next/link";
+import Image from "next/image";
 import ToReactMarkdown from "./toReactMarkdown";
 
 export default function Profile({ profile }) {
+  console.log(profile.fields.portrait.fields.file.details);
   return (
     <div key={profile.sys.id}>
       <h4 className="uppercase">{profile.fields.name}</h4>
       <ToReactMarkdown children={profile.fields.education} additionalClassNames={"mb-2"}/>
       <ToReactMarkdown children={profile.fields.biography} />
       <Link href={`mailto:${profile.fields.email}`}>{profile.fields.email}</Link>
-      {/* <div className="max-w-sm rounded-lg"><Image src={profile.fields.portrait.fields.fileurl} alt={profile.fields.portrait.fields.fileName} /></div> */}
+      <Image src={`https:${profile.fields.portrait.fields.file.url}`} alt={profile.fields.portrait.fields.description} width={profile.fields.portrait.fields.file.details.image.width} height={profile.fields.portrait.fields.file.details.image.height} />
     </div>
   );
 }
