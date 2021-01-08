f798c196508736561fd742ce5c0d6939ddb974ea
balthazarbk <balthazarberlingkelpe@gmail.com>
2021-01-08 10:23:25 +0100

    took away index pictures, making a version so that Paul can add content from contentful with revalidate

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,11 @@ import Layout from "../components/layout";
 import Container from "../components/container";
 
 export default function Home({ homeTitle, categories }) {
-  const imageFileName = [];
-
-  for (let index = 0; index < 19; index++) {
-    imageFileName.push(`square-${index}.jpg`);
-  }
-
-  const allImagesAsElements = imageFileName.map((image, index) => {
-    return <img src={`/images/${image}`} key={index} className="w-2/12" />;
-  });
+  
 
   return (
     <Layout homeTitle={homeTitle} categories={categories}>
-      <div className="flex flex-row flex-wrap">{allImagesAsElements}</div>
+      <Container></Container>
     </Layout>
   );
 }
