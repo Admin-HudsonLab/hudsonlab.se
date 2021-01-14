bcbaf303395ce185ad834c4435d5050b99040c62
balthazarbk <balthazarberlingkelpe@gmail.com>
2021-01-14 16:07:20 +0100

    before install scss

--- components/content.js
+++ components/content.js
@@ -1,6 +1,5 @@
 import ToReactMarkdown from "./toReactMarkdown";
 
 export default function Content({ content }) {
-  /* console.log(content); */
   return <ToReactMarkdown children={content.fields.content} />;
 }
--- components/sectionContainer.js
+++ components/sectionContainer.js
@@ -1,7 +1,10 @@
 export default function SectionContainer({ children, title, slug }) {
+
+
+/*   HERE START TO FIX BACKGROUND COLORS FOR TITLE H3 */
   return (
     <section key={slug} id={slug}>
-      <h3 className="font-ibm font-semibold text-xl bg-blue-200 rounded-lg px-2 py-0.5 ">{title}</h3>
+      <h3 className="font-ibm font-semibold text-2xl bg-beige rounded-lg -mx-1 px-2 py-0.5">{title}</h3>
       {children}
     </section>
   );
