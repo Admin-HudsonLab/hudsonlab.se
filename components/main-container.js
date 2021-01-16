0f0f0c04e212ddd140560e542084565991035f8e
balthazarbk <balthazarberlingkelpe@gmail.com>
2021-01-16 22:31:58 +0100

    fixed it; but at which price?

--- components/main-container.js
+++ components/main-container.js
@@ -1,3 +1,7 @@
 export default function MainContainer({ children, categorySlug }) {
-  return <main id={categorySlug} className="min-h-full z-30 py-4 px-2">{children}</main>;
+  return (
+    <main id={categorySlug} className="min-h-full z-30 py-4 px-2">
+      {children}
+    </main>
+  );
 }
