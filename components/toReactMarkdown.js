3dda67d0d53a30e917b76a4c04ec408abca0f5da
balthazarbk <balthazarberlingkelpe@gmail.com>
2021-01-18 20:02:01 +0100

    before starting to do the sm screen

--- components/toReactMarkdown.js
+++ components/toReactMarkdown.js
@@ -1,12 +1,19 @@
 import ReactMarkdown from "react-markdown";
 import gfm from "remark-gfm";
 
+export function RMImage(props) {
+  return <img src={`https://${props.src}`} alt={props.alt} className="object-contain h-80 w-full mx-auto" />;
+}
+
 export default function ToReactMarkdown({ children, additionalClassNames }) {
+
   return (
     <ReactMarkdown
       plugins={[gfm]}
       children={children}
       className={`prose font-sans${additionalClassNames ? " " + additionalClassNames : ""}`}
+      renderers={{ image: RMImage }}
+      linkTarget="_blank"
     />
   );
 }
